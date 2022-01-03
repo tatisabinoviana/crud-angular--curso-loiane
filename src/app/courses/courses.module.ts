81b256e8d8cb160d51ca35a2a0d2448b8629baf4
Tatiana Viana <tatisabinoviana@gmail.com>
2022-01-03 10:56:02 -0300

    adaptando a recepção dos cursos disponíveis e incluindo uma toobar

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule, 
-    MatTableModule
+    MatTableModule, 
+    MatCardModule, 
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
