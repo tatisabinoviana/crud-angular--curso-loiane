b063f7a73ec80bcd58755aa6785f3cad85a7c668
Tatiana Viana <tatisabinoviana@gmail.com>
2022-01-19 22:14:41 -0300

    iniciado o tratamento de erros, criando component e module para o mesmo, no intuito de reutilizar o codigo em exibição de outros erros futuros

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,8 @@
-import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -19,7 +20,14 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService: CoursesService) {
     //this.courses = [];
     //this.coursesService = new CoursesService();
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError( error => {
+          console.log(error);
+          
+          return of ([]);
+        })
+      );
 
     //this.coursesService.list().subscribe(courses => this.courses = courses);
   }
